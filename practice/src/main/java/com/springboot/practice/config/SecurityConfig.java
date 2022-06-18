971074ea3d029db18d167742566c9309faa5e234
john <deadpoetspoet@naver.com>
2022-06-19 01:14:12 +0900

    2022-06-19

--- practice/src/main/java/com/springboot/practice/config/SecurityConfig.java
+++ practice/src/main/java/com/springboot/practice/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception{
 		http.csrf().disable();
 		http.authorizeRequests()
-			.antMatchers("/")
+			.antMatchers("/**")
 			.authenticated()
 			.anyRequest()
 			.permitAll()
